15c02231e4128b375e2f7f02d8fe98a820b64903
kocoukot <kocou-kot@yandex.ru>
2022-06-18 18:43:00 +0500

    bottom nav bar added

--- app/src/main/java/com/civilcam/ui/MainActivity.kt
+++ app/src/main/java/com/civilcam/ui/MainActivity.kt
@@ -2,15 +2,22 @@ package com.civilcam.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.NavHostFragment
 import com.civilcam.databinding.ActivityMainBinding
+import com.civilcam.ui.common.SupportBottomBar
+import com.civilcam.ui.common.ext.setupWithNavController
 import com.standartmedia.common.ext.castSafe
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var navHost: NavHostFragment? = null
+
+    private val currentVisibleFragment: Fragment?
+        get() = navHost?.childFragmentManager?.fragments?.first()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,15 +35,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         navHost?.let { nav ->
-//            binding.bottomNavigationView.setupWithNavController(
-//                navController = nav.navController,
-//            )
+            binding.bottomNavigationView.setupWithNavController(
+                navController = nav.navController
+            ) {}
         }
     }
 
     private val onBackStackChangedListener by lazy {
         FragmentManager.OnBackStackChangedListener {
-//            binding.bottomNavigationView.isVisible = currentVisibleFragment is SupportBottomBar
+            binding.bottomNavigationView.isVisible = currentVisibleFragment is SupportBottomBar
         }
     }
 
---
+++ app/src/main/java/com/civilcam/ui/alerts/AlertsFragment.kt
@@ -0,0 +1,7 @@
+package com.civilcam.ui.alerts
+
+import androidx.fragment.app.Fragment
+import com.civilcam.R
+import com.civilcam.ui.common.SupportBottomBar
+
+class AlertsFragment : Fragment(R.layout.fragment_alerts), SupportBottomBar
