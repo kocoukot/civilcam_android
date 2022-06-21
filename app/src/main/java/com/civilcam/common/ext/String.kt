fac5e5dd94221c60703ef50cdfb9bd0b22bd941a
kocoukot <kocou-kot@yandex.ru>
2022-06-21 14:13:21 +0500

    phone formatting added

--- app/src/main/java/com/civilcam/common/ext/String.kt
+++ app/src/main/java/com/civilcam/common/ext/String.kt
@@ -3,4 +3,5 @@ package com.civilcam.common.ext
 
 fun String.letters() = filter { it.isLetter() || it == ' ' }
 
+fun String.digits() = filter { it.isDigit() }
 
--- app/src/main/java/com/civilcam/ui/common/compose/InputField.kt
+++ app/src/main/java/com/civilcam/ui/common/compose/InputField.kt
@@ -15,10 +15,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.onFocusEvent
-import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.civilcam.R
+import com.civilcam.common.ext.digits
 import com.civilcam.common.ext.letters
 import com.civilcam.common.theme.CCTheme
 import kotlinx.coroutines.delay
@@ -101,7 +104,7 @@ fun InputField(
                             if (isReversed) CCTheme.colors.white else CCTheme.colors.lightGray,
                             RoundedCornerShape(4.dp)
                         )
-                        .padding(vertical = 12.dp)
+                        .padding(vertical = 14.dp)
                         .padding(start = 12.dp, end = 12.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
@@ -111,7 +114,7 @@ fun InputField(
                     ) {
                         if (inputText.isEmpty()) Text(
                             placeHolder,
-                            modifier = Modifier.padding(bottom = 1.dp),
+                            modifier = Modifier,
                             style = CCTheme.typography.common_text_regular,
                             color = CCTheme.colors.grayText
                         )
@@ -125,10 +128,148 @@ fun InputField(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun PhoneInputField(
+    text: String = "",
+    isReversed: Boolean = false,
+    onValueChanged: (String) -> Unit,
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val viewRequester = BringIntoViewRequester()
+
+    var inputText by remember { mutableStateOf(text) }
+    if (text.isNotEmpty()) inputText = text
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(if (isReversed) CCTheme.colors.lightGray else CCTheme.colors.white)
+            .bringIntoViewRequester(viewRequester)
+            .onFocusEvent {
+                if (it.isFocused) {
+                    coroutineScope.launch {
+                        delay(400)
+                        viewRequester.bringIntoView()
+                    }
+                }
+            }
+    ) {
+
+        Text(
+            stringResource(id = R.string.profile_setup_phone_number_label),
+            color = CCTheme.colors.grayText,
+            style = CCTheme.typography.common_text_small_regular,
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .fillMaxWidth()
+        )
+
+        BasicTextField(
+            visualTransformation = PhoneNumberTransformation(),
+            textStyle = CCTheme.typography.common_text_regular,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 5.dp)
+                .clip(RoundedCornerShape(4.dp)),
+            singleLine = true,
+            value = inputText,
+            onValueChange = { value ->
+                if (value.length < 11) inputText = value.digits()
+                onValueChanged.invoke(inputText.trim())
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Phone
+            ),
+            decorationBox = { innerTextField ->
+                Row(
+                    modifier = Modifier
+                        .background(
+                            if (isReversed) CCTheme.colors.white else CCTheme.colors.lightGray,
+                            RoundedCornerShape(4.dp)
+                        )
+                        .padding(vertical = 14.dp)
+                        .padding(start = 12.dp, end = 12.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        "+1 ",
+                        modifier = Modifier,
+                        style = CCTheme.typography.common_text_regular,
+                        color = CCTheme.colors.grayText
+                    )
+                    Box(
+                        Modifier
+                            .weight(1f)
+                    ) {
+                        if (inputText.isEmpty()) Text(
+                            stringResource(id = R.string.profile_setup_phone_number_placeholder),
+                            modifier = Modifier,
+                            style = CCTheme.typography.common_text_regular,
+                            color = CCTheme.colors.grayText
+                        )
+                        innerTextField()
+                    }
+
+
+                }
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+
+@Preview
+@Composable
+fun PhoneInputFieldPreview() {
+    PhoneInputField(onValueChanged = {})
+}
 
 @Preview
 @Composable
 fun InputFieldPreview() {
     InputField("First name", "Enter First Name",
         onValueChanged = {})
+}
+
+private class PhoneNumberTransformation : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+        return phoneNumFilter(text)
+    }
+}
+
+fun phoneNumFilter(text: AnnotatedString): TransformedText {
+
+    // (XXX) XXX XXXX
+    val trimmed = if (text.text.length >= 10) text.text.substring(0..9) else text.text
+    var out = ""
+    for (i in trimmed.indices) {
+        if (i == 0) out += "("
+        out += trimmed[i]
+        if (i == 2) out += ") "
+        if (i == 5) out += " "
+    }
+
+    val phoneNumberOffsetTranslator = object : OffsetMapping {
+        override fun originalToTransformed(offset: Int): Int {
+            if (offset <= 0) return offset
+            if (offset < 3) return offset + 1
+            if (offset == 3) return offset + 2
+            if (offset <= 6) return offset + 3
+            if (offset < 11) return offset + 4
+            return 14
+
+        }
+
+        override fun transformedToOriginal(offset: Int): Int {
+            if (offset <= 0) return offset
+            if (offset <= 2) return offset - 1
+            if (offset <= 3) return offset - 2
+            if (offset <= 6) return offset - 3
+            if (offset <= 11) return offset - 4
+            return 14
+        }
+    }
+
+    return TransformedText(AnnotatedString(out), phoneNumberOffsetTranslator)
 }
