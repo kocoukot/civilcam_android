71efa0f1b999fb027731b36004f356a5535d63a7
kocoukot <kocou-kot@yandex.ru>
2022-06-28 23:49:41 +0500

    crash fix

--- app/src/main/java/com/civilcam/ui/verification/VerificationFragment.kt
+++ app/src/main/java/com/civilcam/ui/verification/VerificationFragment.kt
@@ -1,12 +1,10 @@
 package com.civilcam.ui.verification
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
-import android.view.inputmethod.InputMethodManager
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.core.os.bundleOf
@@ -16,56 +14,62 @@ import com.civilcam.domain.model.VerificationFlow
 import com.civilcam.ui.common.ext.navController
 import com.civilcam.ui.common.ext.observeNonNull
 import com.civilcam.ui.common.ext.requireArg
+import com.civilcam.ui.terms.TermsFragment
 import com.civilcam.ui.verification.model.VerificationRoute
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
 class VerificationFragment : Fragment() {
-	private val viewModel: VerificationViewModel by viewModel(){
-		parametersOf(verificationFlow, subject)
-	}
-	
-	private val verificationFlow: VerificationFlow by requireArg(ARG_FLOW)
-	private val subject: String by requireArg(ARG_SUBJECT)
-	
-	override fun onCreateView(
-		inflater: LayoutInflater,
-		container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View {
-		activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
-		viewModel.steps.observeNonNull(viewLifecycleOwner) { route ->
-			when (route) {
-				VerificationRoute.GoBack -> { navController.popBackStack() }
-				VerificationRoute.ToNextScreen -> {
-					when(verificationFlow) {
-						VerificationFlow.NEW_EMAIL -> navController.navigate(R.id.termsFragment)
-						VerificationFlow.PHONE -> {}
-						VerificationFlow.RESET_PASSWORD -> {}
-					}
-				}
-			}
-		}
-		
-		return ComposeView(requireContext()).apply {
-			setViewCompositionStrategy(
-				ViewCompositionStrategy.DisposeOnLifecycleDestroyed(
-					viewLifecycleOwner
-				)
-			)
-			setContent {
-				VerificationScreenContent(viewModel)
-			}
-		}
-	}
-	
-	companion object {
-		private const val ARG_FLOW = "verification_flow"
-		private const val ARG_SUBJECT = "verification_subject"
-		
-		fun createArgs(flow: VerificationFlow, subject: String) = bundleOf(
-			ARG_FLOW to flow,
-			ARG_SUBJECT to subject
-		)
-	}
+    private val viewModel: VerificationViewModel by viewModel {
+        parametersOf(verificationFlow, subject)
+    }
+
+    private val verificationFlow: VerificationFlow by requireArg(ARG_FLOW)
+    private val subject: String by requireArg(ARG_SUBJECT)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        viewModel.steps.observeNonNull(viewLifecycleOwner) { route ->
+            when (route) {
+                VerificationRoute.GoBack -> {
+                    navController.popBackStack()
+                }
+                VerificationRoute.ToNextScreen -> {
+                    when (verificationFlow) {
+                        VerificationFlow.NEW_EMAIL -> navController.navigate(
+                            R.id.termsFragment,
+                            TermsFragment.createArgs(false)
+                        )
+                        VerificationFlow.PHONE -> {}
+                        VerificationFlow.RESET_PASSWORD -> {}
+                    }
+                }
+            }
+        }
+
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnLifecycleDestroyed(
+                    viewLifecycleOwner
+                )
+            )
+            setContent {
+                VerificationScreenContent(viewModel)
+            }
+        }
+    }
+
+    companion object {
+        private const val ARG_FLOW = "verification_flow"
+        private const val ARG_SUBJECT = "verification_subject"
+
+        fun createArgs(flow: VerificationFlow, subject: String) = bundleOf(
+            ARG_FLOW to flow,
+            ARG_SUBJECT to subject
+        )
+    }
 }
