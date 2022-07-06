7b858a013c3b2e43421e382110f7787dcd51e9f3
timurcabr <timurcabr@gmail.com>
2022-07-06 23:41:39 +0300

    additional final fixes

--- app/src/main/java/com/civilcam/ui/auth/pincode/PinCodeFragment.kt
+++ app/src/main/java/com/civilcam/ui/auth/pincode/PinCodeFragment.kt
@@ -23,7 +23,7 @@ import org.koin.core.parameter.parametersOf
 
 class PinCodeFragment : Fragment() {
 	private val viewModel: PinCodeViewModel by viewModel{
-		parametersOf(PinCodeFlow.CURRENT_PIN_CODE)
+		parametersOf(pinCodeFlow)
 	}
 	
 	private val pinCodeFlow: PinCodeFlow by requireArg(ARG_FLOW)
