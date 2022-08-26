ae22326bbfd431c42b6d1387f6135d7bee98d448
kocoukot <56508003+kocoukot@users.noreply.github.com>
2022-08-26 15:56:18 +0400

    google sign added

--- app/src/main/java/com/civilcam/data/network/model/request/auth/GoogleOAuthRequest.kt
+++ app/src/main/java/com/civilcam/data/network/model/request/auth/GoogleOAuthRequest.kt
@@ -8,6 +8,5 @@ class GoogleOAuthRequest(
     @SerializedName("grant_type") private val grantType: String = "authorization_code",
     @SerializedName("client_id") private val clientId: String = BuildConfig.GOOGLE_WEB_CLIENT_ID,
     @SerializedName("client_secret") private val clientSecret: String = BuildConfig.GOOGLE_CLIENT_SECRET,
-    @SerializedName("redirect_uri") private val redirect_uri: String = "https://api.staging.civilcam.cloud",
-    @SerializedName("scope") private val scope: String = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
+    @SerializedName("redirect_uri") private val redirect_uri: String = "",
 )
