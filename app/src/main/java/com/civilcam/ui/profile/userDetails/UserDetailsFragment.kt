e9ea998f049900ed2c0c88aa54d7b8a21b409722
kocoukot <56508003+kocoukot@users.noreply.github.com>
2022-09-07 11:43:07 +0400

    user detail screen issue fix

--- app/src/main/java/com/civilcam/ui/profile/userDetails/UserDetailsFragment.kt
+++ app/src/main/java/com/civilcam/ui/profile/userDetails/UserDetailsFragment.kt
@@ -10,7 +10,6 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import com.civilcam.ui.common.ext.navController
 import com.civilcam.ui.common.ext.observeNonNull
-import com.civilcam.ui.common.ext.requireArg
 import com.civilcam.ui.profile.userDetails.model.UserDetailsRoute
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
@@ -20,7 +19,7 @@ class UserDetailsFragment : Fragment() {
         parametersOf(userId)
     }
 
-    private val userId by requireArg<Int>(ARG_USER_ID)
+    private val userId = 120// by requireArg<Int>(ARG_USER_ID)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
--- app/src/main/java/com/civilcam/ui/profile/userDetails/UserDetailsScreenContent.kt
+++ app/src/main/java/com/civilcam/ui/profile/userDetails/UserDetailsScreenContent.kt
@@ -2,15 +2,14 @@
 
 package com.civilcam.ui.profile.userDetails
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
-import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -20,7 +19,6 @@ import com.civilcam.domainLayer.model.AlertDialogTypes
 import com.civilcam.domainLayer.model.guard.GuardianStatus
 import com.civilcam.ui.common.alert.AlertDialogComp
 import com.civilcam.ui.common.compose.BackButton
-import com.civilcam.ui.common.compose.TopAppBarContent
 import com.civilcam.ui.common.loading.DialogLoadingContent
 import com.civilcam.ui.profile.userDetails.content.UserDetailsSection
 import com.civilcam.ui.profile.userDetails.content.UserRequestSection
@@ -28,6 +26,7 @@ import com.civilcam.ui.profile.userDetails.model.StopGuardAlertType
 import com.civilcam.ui.profile.userDetails.model.UserDetailsActions
 import com.google.accompanist.pager.ExperimentalPagerApi
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun UserDetailsScreenContent(viewModel: UserDetailsViewModel) {
 
@@ -63,42 +62,43 @@ fun UserDetailsScreenContent(viewModel: UserDetailsViewModel) {
             }
         )
     }
-    Scaffold(
-        backgroundColor = CCTheme.colors.lightGray,
+    Surface(
         modifier = Modifier.fillMaxSize(),
-        topBar = {
-            TopAppBarContent(
-                title = "",
-                navigationItem = {
-                    BackButton {
-                        viewModel.setInputActions(UserDetailsActions.ClickGoBack)
-                    }
-                })
-        }
+        color = CCTheme.colors.lightGray,
     ) {
-        state.value.data?.let { data ->
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(CCTheme.colors.white),
-            ) {
-                UserDetailsSection(
-                    userData = data,
-                    myGuardenceChange = viewModel::setInputActions
-                )
+        Box(
+            Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopEnd
+        ) {
 
-                if (data.personStatus?.status == GuardianStatus.PENDING || data.personStatus?.status == GuardianStatus.DECLINED) {
-                    Divider(
-                        color = CCTheme.colors.lightGray, modifier = Modifier
-                            .height(20.dp)
+
+            state.value.data?.let { data ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(CCTheme.colors.white),
+                ) {
+                    UserDetailsSection(
+                        userData = data,
+                        myGuardenceChange = viewModel::setInputActions
                     )
-                    UserRequestSection(
-                        data.personFullName,
-                        stringResource(id = R.string.user_details_request_text)
-                    ) {
-                        viewModel.setInputActions(UserDetailsActions.ClickRequestAnswer(it))
+
+                    if (data.personStatus?.status == GuardianStatus.PENDING || data.personStatus?.status == GuardianStatus.DECLINED) {
+                        Divider(
+                            color = CCTheme.colors.lightGray, modifier = Modifier
+                                .height(20.dp)
+                        )
+                        UserRequestSection(
+                            data.personFullName,
+                            stringResource(id = R.string.user_details_request_text)
+                        ) {
+                            viewModel.setInputActions(UserDetailsActions.ClickRequestAnswer(it))
+                        }
                     }
                 }
+            }
+            BackButton(modifier = Modifier.align(Alignment.TopStart)) {
+                viewModel.setInputActions(UserDetailsActions.ClickGoBack)
             }
         }
     }
