2d356a7f697a503e598c6f98e45ac2587a0bb7ff
timurcabr <timurcabr@gmail.com>
2022-09-08 14:17:54 +0300

    some changes on no match state

--- app/src/main/java/com/civilcam/ui/auth/pincode/model/PinCodeState.kt
+++ app/src/main/java/com/civilcam/ui/auth/pincode/model/PinCodeState.kt
@@ -10,11 +10,8 @@ data class PinCodeState(
 	val confirmPinCode: String = "",
 	val currentPinCode: String = "",
 	
-	var isDataLoaded: Boolean = false,
-	
 	val noMatch: Boolean = false,
 	val currentNoMatch: Boolean = false,
-	val newPinNoMatch: Boolean = false,
 	
 	val screenState: PinCodeFlow = PinCodeFlow.CREATE_PIN_CODE,
 ) : ComposeFragmentState {
