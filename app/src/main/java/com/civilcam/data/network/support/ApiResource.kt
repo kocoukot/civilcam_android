a5414a71d1a1ce9ded62b9d40c5818ac4950231d
kocoukot <56508003+kocoukot@users.noreply.github.com>
2022-09-14 18:43:27 +0400

    some code clean up and refactor

--- app/src/main/java/com/civilcam/data/network/support/ApiResource.kt
+++ app/src/main/java/com/civilcam/data/network/support/ApiResource.kt
@@ -4,10 +4,7 @@ import com.civilcam.domainLayer.ServiceException
 
 sealed class Resource<out T> {
     data class Success<out T>(val value: T) : Resource<T>()
-    data class Failure(
-        val serviceException: ServiceException,
-    ) : Resource<Nothing>() {
-
+    data class Failure(val serviceException: ServiceException) : Resource<Nothing>() {
         fun checkIfLogOut(needToLogOut: () -> Unit) {
             if (this.serviceException.isForceLogout) {
                 needToLogOut.invoke()
