1340be99d034d4e4beb81280590e0632d2756a7d
kocoukot <56508003+kocoukot@users.noreply.github.com>
2022-09-15 17:03:59 +0400

    alerts api adding

--- domainLayer/src/main/java/com/civilcam/domainLayer/repos/MockRepository.kt
+++ domainLayer/src/main/java/com/civilcam/domainLayer/repos/MockRepository.kt
@@ -1,16 +1,9 @@
 package com.civilcam.domainLayer.repos
 
 import com.civilcam.domainLayer.model.SubscriptionPlan
-import com.civilcam.domainLayer.model.alerts.AlertModel
-import com.civilcam.domainLayer.model.alerts.AlertType
 import com.civilcam.domainLayer.model.alerts.GuardianAlertInformation
 
 interface MockRepository {
-
-
-    suspend fun getAlerts(): List<AlertModel>
-
-    suspend fun getHistoryAlert(alertType: AlertType): List<AlertModel>
 
     fun getCurrentSubscriptionPlan(): SubscriptionPlan
 
