2742502a5cd49dc509bb9d63132e7b4caff3d331
kocoukot <kocou-kot@yandex.ru>
2022-12-08 20:16:58 +0500

    some base fragment code refactor

--- alert-feature/src/main/java/com/civilcam/alert_feature/history/AlertsHistoryFragment.kt
+++ alert-feature/src/main/java/com/civilcam/alert_feature/history/AlertsHistoryFragment.kt
@@ -1,10 +1,7 @@
 package com.civilcam.alert_feature.history
 
 import androidx.compose.runtime.Composable
-import com.civilcam.alert_feature.history.model.AlertHistoryRoute
 import com.civilcam.ext_features.arch.BaseFragment
-import com.civilcam.ext_features.compose.ComposeFragmentRoute
-import com.civilcam.ext_features.ext.callPhone
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AlertsHistoryFragment : BaseFragment<AlertsHistoryViewModel>() {
@@ -12,12 +9,4 @@ class AlertsHistoryFragment : BaseFragment<AlertsHistoryViewModel>() {
 
     override val screenContent: @Composable (AlertsHistoryViewModel) -> Unit =
         { AlertsListScreenContent(viewModel) }
-
-    override fun observeData(composeRoute: ((ComposeFragmentRoute) -> Unit)?) {
-        super.observeData { route ->
-            when (route) {
-                is AlertHistoryRoute.CallUser -> callPhone(route.phoneNumber)
-            }
-        }
-    }
 }
