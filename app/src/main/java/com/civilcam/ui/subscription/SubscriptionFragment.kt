83c96c239592edc62796c762910f4878e6ccd22c
kocoukot <kocou-kot@yandex.ru>
2022-12-20 13:03:02 +0500

    subscription flow prepare

--- app/src/main/java/com/civilcam/ui/subscription/SubscriptionFragment.kt
+++ app/src/main/java/com/civilcam/ui/subscription/SubscriptionFragment.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import com.android.billingclient.api.*
 import com.civilcam.R
 import com.civilcam.common.ext.navigateToRoot
 import com.civilcam.domainLayer.model.subscription.UserSubscriptionState
@@ -22,6 +23,7 @@ import com.civilcam.ui.auth.pincode.model.PinCodeFlow
 import com.civilcam.ui.subscription.model.SubscriptionRoute
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
+import timber.log.Timber
 
 class SubscriptionFragment : Fragment() {
 	private val viewModel: SubscriptionViewModel by viewModel {
@@ -29,6 +31,23 @@ class SubscriptionFragment : Fragment() {
 	}
 
 	private val useSubState: UserSubscriptionState by requireArg(ARG_FLOW)
+
+
+	private val purchasesUpdatedListener =
+		PurchasesUpdatedListener { billingResult, purchases ->
+		}
+
+	private val billingClient by lazy {
+		BillingClient.newBuilder(requireContext())
+			.setListener(purchasesUpdatedListener)
+			.enablePendingPurchases()
+			.build()
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		billStartConnection()
+	}
 
 	override fun onCreateView(
 		inflater: LayoutInflater,
@@ -70,6 +89,71 @@ class SubscriptionFragment : Fragment() {
 	override fun onStop() {
 		super.onStop()
 		showSystemUI()
+	}
+
+	private fun billStartConnection() {
+		billingClient.startConnection(object : BillingClientStateListener {
+			override fun onBillingSetupFinished(billingResult: BillingResult) {
+				if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+
+				}
+			}
+
+			override fun onBillingServiceDisconnected() {
+				billStartConnection()
+			}
+		})
+	}
+
+	private fun getProductsList() {
+		val queryProductDetailsParams =
+			QueryProductDetailsParams.newBuilder()
+				.setProductList(
+					listOf(
+						QueryProductDetailsParams.Product.newBuilder()
+//							.setProductId(ONE_TRY_PRODUCT_ID)
+//							.setProductType(BillingClient.ProductType.INAPP)
+							.build(),
+
+						QueryProductDetailsParams.Product.newBuilder()
+//							.setProductId(ONE_DAY_PRODUCT_ID)
+//							.setProductType(BillingClient.ProductType.INAPP)
+							.build()
+					)
+				)
+				.build()
+
+		billingClient.queryProductDetailsAsync(queryProductDetailsParams) { billingResult, productDetailsList ->
+			if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+				Timber.i("productDetailsList $productDetailsList")
+//				viewModel.onBillsGot(productDetailsList.toList())
+			}
+		}
+	}
+
+	private fun launchBillFlow(product: ProductDetails) {
+		val productDetailsParamsList = listOf(
+			BillingFlowParams.ProductDetailsParams.newBuilder()
+				.setProductDetails(product)
+				.build()
+		)
+
+		val billingFlowParams = BillingFlowParams.newBuilder()
+			.setProductDetailsParamsList(productDetailsParamsList)
+			.build()
+		billingClient.launchBillingFlow(requireActivity(), billingFlowParams)
+	}
+
+	private fun handlePurchase(purchase: Purchase) {
+		val consumeParams =
+			ConsumeParams.newBuilder()
+				.setPurchaseToken(purchase.purchaseToken)
+				.build()
+//		lifecycleScope.launch {
+//			withContext(Dispatchers.IO) {
+//				billingClient.consumePurchase(consumeParams)
+//			}
+//		}
 	}
 
 	companion object {
